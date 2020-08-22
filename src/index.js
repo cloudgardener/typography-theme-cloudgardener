a066b6845e4fd29e9a436412970a5bc2248ed4a3
Niko Virtala <niko.virtala@vr.fi>
2020-08-22 14:11:43 +0300

    fixed width version.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,18 @@ import gray from "gray-percentage";
 
 const theme = {
   title: "Cloud Gardener",
-  baseFontSize: "18px",
+  baseFontSize: "16px",
   baseLineHeight: 1.5,
   scale: 2.25,
   googleFonts: [
     {
-      name: "Source Sans Pro",
-      styles: ["700"]
-    },
-    {
-      name: "Source Serif Pro",
-      styles: ["400", "400i", "700"]
-    },
-    {
       name: "Source Code Pro",
-      styles: ["400"]
-    }
+      styles: ["400", "400i", "600"],
+    },
   ],
-  headerFontFamily: ["Source Sans Pro", "sans-serif"],
-  bodyFontFamily: ["Source Serif Pro", "serif"],
-  codeFontFamily: ["Source Code Pro", "monospace"],
+  headerFontFamily: ["Source Code Pro", "Menlo", "monospace"],
+  bodyFontFamily: ["Source Code Pro", "Menlo", "monospace"],
+  codeFontFamily: ["Source Code Pro", "Menlo", "monospace"],
   bodyColor: "hsla(0,0%,0%,0.8)",
   headerWeight: 700,
   bodyWeight: 400,
@@ -37,41 +29,41 @@ const theme = {
       ...scale(-2 / 8),
       fontFamily: options.bodyFontFamily.join(","),
       fontWeight: options.bodyWeight,
-      textTransform: "uppercase"
+      textTransform: "uppercase",
     },
     a: {
-      color: "#00ab6b"
+      color: "#00ab6b",
     },
     "a:hover": {
-      color: "#038252"
+      color: "#038252",
     },
     blockquote: {
       fontStyle: "italic",
       borderLeft: `${rhythm(1 / 6)} solid`,
-      borderColor: gray(93),
+      borderColor: "#ff4aaf",
       paddingTop: rhythm(1 / 3),
       paddingBottom: rhythm(1 / 3),
       paddingLeft: rhythm(2 / 3),
-      paddingRight: rhythm(2 / 3)
+      paddingRight: rhythm(2 / 3),
     },
     "blockquote > :last-child": {
-      marginBottom: 0
+      marginBottom: 0,
     },
     "blockquote cite": {
       ...adjustFontSizeTo(options.baseFontSize),
       color: gray(54, 204),
       fontWeight: options.bodyWeight,
-      fontStyle: "normal"
+      fontStyle: "normal",
     },
     pre: {
       fontFamily: options.codeFontFamily.join(","),
-      fontWeight: options.codeWeight
+      fontWeight: options.codeWeight,
     },
     code: {
       fontFamily: options.codeFontFamily.join(","),
-      fontWeight: options.codeWeight
-    }
-  })
+      fontWeight: options.codeWeight,
+    },
+  }),
 };
 
 export default theme;
